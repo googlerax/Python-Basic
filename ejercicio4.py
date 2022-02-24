fb2d9bde0071bce5163542c33fcc468b609fbc28
googlerax <46288252+googlerax@users.noreply.github.com>
2022-02-24 19:31:20 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,5 +2,6 @@ variable = 'esto es una cadena de texto.'
 print(variable)
 tipo = type(variable)
 print('La variable es del tipo:',tipo)
-variable = 74
-print('La variable ahora es',variable,type(variable))
+
+variable[1] = 'x'
+print(variable)       #TypeError: 'str' object does not support item assignment      String es un tipo de dato inmutable,.
