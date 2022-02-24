cf729bc5f0144fec692c519bc5d167d8651362d2
googlerax <46288252+googlerax@users.noreply.github.com>
2022-02-24 20:07:29 -0300

    Add files via upload
    Ejercicio 5 de python. IDE PyCharm.

---
+++ Ejercicio5.py
@@ -0,0 +1,25 @@
+nombre = 'Maria'
+apellido = 'Sanchez'
+edad = 36
+email = 'mgrrghs@gmail.com'
+telefono = '1578663948'
+casada = False
+hijos = False
+
+amigos = ['Ana','Alberto','Miguel','Pia']
+
+pelisVistas = {
+    'peli1':'Troya',
+    'peli2':'Hombres de honor',
+    'peli3':'Batman'
+}
+
+print(nombre,apellido,edad,'Años')
+print('Edad:',edad,' ','Email:',email)
+print('Casada:',casada)
+print('Hijos:',hijos)
+
+print('Sus amigos son:',amigos[0],amigos[1],amigos[2],'y',amigos[3],)
+print('Peliculas vistas:',pelisVistas['peli1'],pelisVistas['peli2'],'y',pelisVistas['peli3'])
+
+
