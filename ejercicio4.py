24b85204d4682f15358665795f6af3e3af7f150f
googlerax <46288252+googlerax@users.noreply.github.com>
2022-02-24 19:06:26 -0300

    Add files via upload
    Ejercicio 4 de python. IDE PyCharm.

---
+++ ejercicio4.py
@@ -0,0 +1,6 @@
+variable = 'esto es una cadena de texto.'
+print(variable)
+tipo = type(variable)
+print('La variable es del tipo:',tipo)
+variable = 74
+print('La variable ahora es',variable,type(variable))
