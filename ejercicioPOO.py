96288d17e0c8ac04add98849668a6ea7fc53bd3a
googlerax <46288252+googlerax@users.noreply.github.com>
2022-02-26 18:28:54 -0300

    Add files via upload

---
+++ ejercicioPOO.py
@@ -0,0 +1,17 @@
+#objeto: representacion de algo del mundo real.
+class Vehiculo():    #clase
+    color='azul'     #atributos o propiedades
+    ruedas=4
+    puertas=4
+
+
+
+class Coche(Vehiculo):   #clase Coche hereda de vehiculo
+    velocidad=200
+    cilindradas= 973
+
+
+
+auto=Coche()  #instancia de la clase coche.
+
+print(auto.color,auto.ruedas,auto.puertas,auto.velocidad,auto.cilindradas)
