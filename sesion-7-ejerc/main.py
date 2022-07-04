ee38bd800b49b445b3473d996359f22d20d6858d
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-04 19:25:56 -0300

    Add files via upload

---
+++ sesion-7-ejerc/main.py
@@ -0,0 +1,20 @@
+from operations import *
+
+suma =sumar(48,0.5)
+
+print('La suma es de: ',suma)
+
+
+resta= restar(400,96)
+
+print('La resta es de: ',resta)
+
+
+multi = multiplicar(85,36)
+
+print('La multiplicacion da: ',multi)
+
+
+div = dividir(45,100)
+
+print('La division es de: ',div)
