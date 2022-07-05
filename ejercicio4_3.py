2fccb74910d76ffbdb09925e41f30b16bdbca4cb
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-05 11:51:42 -0300

    Add files via upload

---
+++ ejercicio4_3.py
@@ -0,0 +1,16 @@
+#Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+
+numeros = range(1, 101)
+for alReves in reversed(numeros):
+    print(alReves)
+
+
+
+
+list1 = [7, 70, 8, 44, 1, 0]
+for num in list1:
+    if num % 2 != 0:
+        print(num, end=" ")
+
+
+
