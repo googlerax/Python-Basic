9842681ec1796aee50e9d1e593ba493b2147b3aa
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-05 11:39:48 -0300

    Update and rename Ejercicio5.py to Ejercicio3_1.py

--- Ejercicio3_1.py
+++ Ejercicio3_1.py
@@ -1,8 +1,22 @@
-nombre = 'Maria'
+#Para este ejercicio tenéis que crear en la consola de python variables que representen los siguientes datos de un contacto:
+# Nombre
+# Apellidos
+# Edad
+# Email
+# Teléfono
+# Casado (verdadero o falso)
+# Con Hijos (verdadero o falso)
+# Lista de amigos
+# Películas vistas (diccionario con clave y valor. El valor será el título de la película)
+# Una vez hayas creado todas las variables, muéstralas por consola haciendo uso de la función print().
+
+
+
+nombre = 'Marta'
 apellido = 'Sanchez'
 edad = 36
 email = 'mgrrghs@gmail.com'
-telefono = '1578663948'
+telefono = '1578663989'
 casada = False
 hijos = False
 
