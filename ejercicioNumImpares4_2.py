64b9208c3a8c41def3522f5b40339732a732c74f
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-05 11:55:13 -0300

    Update ejercicioNumImpares4_2.py

--- ejercicioNumImpares4_2.py
+++ ejercicioNumImpares4_2.py
@@ -1,3 +1,8 @@
+#Escribe un programa capaz de mostrar todos los números impares desde un número de inicio y otro final.
+# Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, el programa debe imprimir por consola: [3, 5, 7]
+
+
+
 numInicial=5
 numFinal=17
 
