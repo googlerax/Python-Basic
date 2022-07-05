b3d12e5b68de535a64450ce1a8bd99d67642a343
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-05 11:35:10 -0300

    Update ejercicio1_1.py

--- ejercicio1_1.py
+++ ejercicio1_1.py
@@ -1,3 +1,9 @@
+# #Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+# Hola, soy [tu_nombre]
+# Estoy empezando el curso de python
+# Espero aprender mucho
+
+
 miNombre = 'Griselda.'
 print('hola, soy', miNombre)
 print('estoy empezando el curso de python')
