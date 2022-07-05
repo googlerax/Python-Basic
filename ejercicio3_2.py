c8f9e204974bbcd98cc7323ff81098cda49c33a0
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-05 11:42:32 -0300

    Update and rename ejercicio6.py to ejercicio3_2.py

--- ejercicio3_2.py
+++ ejercicio3_2.py
@@ -1,4 +1,12 @@
 
+#Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros), 
+# calcule el índice de masa corporal y lo almacene en una variable, 
+# e imprima por pantalla la frase Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales. 
+# Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+
+
+
+
 import math
 
 #Calculadora de IMC (Indice de masa corporal)
@@ -12,7 +20,6 @@ peso = input('Introduce tu peso en kg')
 estatura = input('Introduce tu estatura en metros')
 
 imc = round(float(peso) / float(estatura) ** 2,2)
-
 
 
 print('Tu indice de masa corporal es:',str(imc))
