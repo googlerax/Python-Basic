ae456e7a62413780cc7fc415e5e8804b9b704967
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-05 11:37:21 -0300

    Update and rename ejercicio4.py to ejercicio2_2.py

--- ejercicio2_2.py
+++ ejercicio2_2.py
@@ -1,3 +1,5 @@
+#Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
+
 variable = 'esto es una cadena de texto.'
 print(variable)
 tipo = type(variable)
