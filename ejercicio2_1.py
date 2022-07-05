b1efd952a52c746153f8c4b851fd2937c39fe810
googlerax <46288252+googlerax@users.noreply.github.com>
2022-07-05 11:36:24 -0300

    Update and rename ejercicio3.py to ejercicio2_1.py

---
+++ ejercicio2_1.py
@@ -0,0 +1,7 @@
+# En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, 
+# tendrás que imprimir también el tipo de la variable por consola.
+
+variable = 'esto es una cadena de texto.'
+print(variable)
+tipo = type(variable)
+print('La variable es del tipo:',tipo)
--- ejercicio3.py
+++
@@ -1,4 +0,0 @@
-variable = 'esto es una cadena de texto.'
-print(variable)
-tipo = type(variable)
-print('La variable es del tipo:',tipo)
